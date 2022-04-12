bc684590baef8f2c0dd11339d20836378c474fc7
MartinDeFrancesco <defrancescoma@gmail.com>
2022-04-11 18:11:50 -0700

    Martin and Seba test contribution

---
+++ martin_seba_test/README.md
@@ -0,0 +1,16 @@
+# Test Suit for Holberton's Simple Shell #
+Here you can test our own shell made for a Linux system
+
+## Enviroment ##
+* Shell script
+* chmod u+x
+
+## Usage ##
+First clone the following repo `git clone https://github.com/Nachop51/shell_tests.git`. Then compile the program with `gcc -g -Wall -Werror -Wextra -pedantic -std=gnu89 *.c -o hsh` and then run the script with `sh tests_file.sh`
+
+## Verify and compare ##
+After this has been done, compare both output's. If they are the same, congratulations
+
+### Authors ###
+* Martín De Francesco
+* Sebastián Freire
---
+++ martin_seba_test/testsuite.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+clear
+echo ""
+echo "=============Test 1============="
+echo "Command tested: "ls -l" | ./hsh"
+echo "---------------------------------"
+echo "Correct output: "
+echo "ls -l" | /bin/sh
+echo "---------------------------------"
+echo "Your output: "
+echo "ls -l" | ./hsh
+echo "================================="
+echo ""
+echo "=============Test 2============="
+echo "Command tested: "whoami" | ./hsh"
+echo "---------------------------------"
+echo "Correct output: "
+echo "whoami" | /bin/sh
+echo "---------------------------------"
+echo "Your output: "
+echo "whoami" | ./hsh
+echo "================================="
+echo ""
+echo "=============Test 3============="
+echo "Command tested: "pwd" | ./hsh"
+echo "---------------------------------"
+echo "Correct output: "
+echo "pwd" | /bin/sh
+echo "---------------------------------"
+echo "Your output: "
+echo "pwd" | ./hsh
+echo "================================="
+echo ""
+echo "=============Test 4============="
+echo "Command tested: "NYC" | ./hsh"
+echo "---------------------------------"
+echo "Correct output: "
+echo "NYC" | /bin/sh
+echo "---------------------------------"
+echo "Your output: "
+echo "NYC" | ./hsh
+echo "================================="
+echo ""
+echo "=============Test 5============="
+echo "Command tested: "echo "Hello World"" | ./hsh"
+echo "---------------------------------"
+echo "Correct output: "
+echo "echo "Hello World"" | /bin/sh
+echo "---------------------------------"
+echo "Your output: "
+echo "echo "Hello World"" | ./hsh
+echo "================================="
+echo ""
+echo "=============Test 5============="
+echo "Command tested: "touch todoOK" | ./hsh"
+echo "---------------------------------"
+echo "Correct output: "
+echo "touch todoOK" | /bin/sh
+echo "---------------------------------"
+echo "Your output: "
+echo "touch todoOK" | ./hsh
+echo "================================="
+echo ""
